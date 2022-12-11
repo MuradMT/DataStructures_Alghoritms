4bf20b05208ea6cb645748bf9a82ce2ff3cb7954
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-11 22:00:06 +0400

    Leetcode-ValidParanthess

--- DataStructures_Alghoritms/Program.cs
+++ DataStructures_Alghoritms/Program.cs
@@ -1,4 +1,25 @@
 ﻿using static System.Console;
 using static System.Math;
 using static System.Random;
+static bool IsValid(string s)
+{
+    Dictionary<char, char> dic = new Dictionary<char, char>();
+    dic.Add('(', ')');
+    dic.Add('{', '}');
+    dic.Add('[', ']');
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (dic.Keys.Contains(s[i]))
+        {
+            WriteLine(dic[s[i]]);
+            if (!dic.Values.Contains(dic[s[i]]))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+string x = "{]";
+WriteLine(IsValid(x)?"duz":"sehv");
 
